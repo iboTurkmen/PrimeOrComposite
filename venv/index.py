b388eeb6e3f86291515fc53387cc052042f39c5b
iboturkmen <iboTurkmen92@outlook.com>
2020-05-13 01:39:36 +0300

    1) check if input number is prime or composite 2) write factors for both prime and composite 3) 1st method number of iteration  is: 100

--- venv/index.py
+++ venv/index.py
@@ -1,6 +1,24 @@
 
+def prime(inputNum):
+    factors = ""
+    iteration = 1
+    for number in range(2, int(inputNum)):
+        iteration += 1
+    print(inputNum + " is prime number and factors are -> " + inputNum)
+    print("With 1st method number of iteration  is: " + str(iteration))
+    main()
 
 
+def composite(inputNum):
+    factors = ""
+    iteration = 1
+    for number in range(2, int(inputNum)):
+        iteration += 1
+        if int(inputNum) % number == 0:
+            factors += str(number)+","
+    print(inputNum+" is composite number and factors are -> "+factors+inputNum)
+    print("With 1st method number of iteration  is: " + str(iteration))
+    main()
 
 
 def checkNum(inputNum):
@@ -11,13 +29,27 @@ def checkNum(inputNum):
         print(inputNum + " is neither prime nor composite.")
         main()
     else:
-        calculate(inputNum)
+        if int(inputNum) == 2:
+            print(inputNum + " is prime number and factors are -> " + inputNum)
+            print("With 1st method number of iteration  is: 1")
+            main()
+        else:
+            for number in range(2, int(inputNum)):
+                if int(inputNum) % number == 0:
+                    composite(inputNum)
+                    break
+                else:
+                    prime(inputNum)
+                    break
 
 
 def main():
+    print("For Exit please enter 'EXIT' or 'exit'")
     inputNum = input("Please Enter a Number : ")
     if inputNum.upper() == "EXIT":
         exit()
+    if inputNum == "":
+        main()
     if int(inputNum) < 0:
         print("Please Enter Positive Numbers only!!!")
         main()
